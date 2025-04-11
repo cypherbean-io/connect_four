94d86211cd430c6ce9fb4b0b085e0b74fd55efe0
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-04-10 20:40:51 -0500

    Add methods to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,27 @@ class Board
   def initialize
     @grid = Array.new(7) { Array.new(6) }
   end
+
+  def place_piece(column, piece)
+    return false unless valid_column?(column)
+
+    row = next_available_row(column)
+    return false if row.nil?
+
+    @grid[column][row] = piece
+    true
+  end
+
+  private
+
+  def valid_column?(column)
+    column.between?(0, 6)
+  end
+
+  def next_available_row(column)
+    @grid[column].each_with_index do |cell, row|
+      return row if cell.nil?
+    end
+    nil
+  end
 end
