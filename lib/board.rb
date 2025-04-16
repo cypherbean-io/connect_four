241f444f3c3f5b2ac5bc40a97221b6cdbcb69030
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-04-15 20:31:37 -0500

    Add #winning_move? and check methods

--- lib/board.rb
+++ lib/board.rb
@@ -40,6 +40,36 @@ class Board
     result
   end
 
+  def winning_move?(column, piece)
+    return false unless valid_column?(column)
+
+    # For checking the winning move, we need to check all positions where this piece exists
+    # rather than just finding the first one
+    rows_with_piece = []
+    @grid[column].each_with_index do |cell, row|
+      rows_with_piece << row if cell == piece
+    end
+
+    return false if rows_with_piece.empty?
+
+    # Check each position where this piece exists in the column
+    rows_with_piece.each do |row|
+      # Check horizontal
+      return true if check_horizontal(column, row, piece)
+
+      # Check vertical
+      return true if check_vertical(column, row, piece)
+
+      # Check diagonal (rising)
+      return true if check_diagonal_rising(column, row, piece)
+
+      # Check diagonal (falling)
+      return true if check_diagonal_falling(column, row, piece)
+    end
+
+    false
+  end
+
   private
 
   def valid_column?(column)
@@ -51,5 +81,86 @@ class Board
       return row if cell.nil?
     end
     nil
+  end
+
+  def check_horizontal(column, row, piece)
+    # Check to the left
+    left_count = 0
+    current_col = column - 1
+    while current_col >= 0 && @grid[current_col][row] == piece
+      left_count += 1
+      current_col -= 1
+    end
+
+    # Check to the right
+    right_count = 0
+    current_col = column + 1
+    while current_col < 7 && @grid[current_col][row] == piece
+      right_count += 1
+      current_col += 1
+    end
+
+    # The original piece plus the consecutive ones to the left and right
+    left_count + 1 + right_count >= 4
+  end
+
+  def check_vertical(column, row, piece)
+    # Only need to check below (pieces can't be floating)
+    down_count = 0
+    current_row = row - 1
+    while current_row >= 0 && @grid[column][current_row] == piece
+      down_count += 1
+      current_row -= 1
+    end
+
+    down_count + 1 >= 4
+  end
+
+  def check_diagonal_rising(column, row, piece)
+    # Check down-left
+    down_left_count = 0
+    c = column - 1
+    r = row - 1
+    while c >= 0 && r >= 0 && @grid[c][r] == piece
+      down_left_count += 1
+      c -= 1
+      r -= 1
+    end
+
+    # Check up-right
+    up_right_count = 0
+    c = column + 1
+    r = row + 1
+    while c < 7 && r < 6 && @grid[c][r] == piece
+      up_right_count += 1
+      c += 1
+      r += 1
+    end
+
+    down_left_count + 1 + up_right_count >= 4
+  end
+
+  def check_diagonal_falling(column, row, piece)
+    # Check down-right
+    down_right_count = 0
+    c = column + 1
+    r = row - 1
+    while c < 7 && r >= 0 && @grid[c][r] == piece
+      down_right_count += 1
+      c += 1
+      r -= 1
+    end
+
+    # Check up-left
+    up_left_count = 0
+    c = column - 1
+    r = row + 1
+    while c >= 0 && r < 6 && @grid[c][r] == piece
+      up_left_count += 1
+      c -= 1
+      r += 1
+    end
+
+    down_right_count + 1 + up_left_count >= 4
   end
 end
